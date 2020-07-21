a9c45b79e177b8e2d597a2bfbb0726e070de6408
Thomas Gilgenast <thomas@vant.ai>
2020-07-21 18:16:46 -0400

    minor fixes for PEP 503 compat

--- proxy/__init__.py
+++ proxy/__init__.py
@@ -23,12 +23,18 @@ class CachingS3Proxy(object):
             start_response(status, response_headers)
             return ['Caching S3 Proxy']
 
-        path_info = path_info.lstrip('/')
+        # this used to be lstrip
+        # for our purposes, a key ending in / should have the trailing /
+        # stripped here; if the resulting key is a "directory" we will append
+        # "/index.html" later
+        path_info = path_info.strip('/')
         (bucket, key) = path_info.split('/', 1)
         try:
             s3_result = self.fetch_s3_object(bucket, key)
             status = '200 OK'
-            response_headers = [('Content-type', 'text/plain')]
+            # pip expects that index.html will be served as text/html and
+            # doesn't care what we serve the actual wheel as
+            response_headers = [('Content-type', 'text/html')]
         except botocore.exceptions.ClientError as ce:
             s3_result = ce.response['Error']['Message']
             status = '404 NOT FOUND'
@@ -46,8 +52,11 @@ class CachingS3Proxy(object):
             return self.cache[cache_key]
         except KeyError:
             self.logger.debug('cache miss for %s' % cache_key)
-
-            obj = self.s3.Object(bucket, key).get()
+            # if a key doesn't exist, append "/index.html" and try again
+            try:
+                obj = self.s3.Object(bucket, key).get()
+            except self.s3.meta.client.exceptions.NoSuchKey:
+                obj = self.s3.Object(bucket, key + '/index.html').get()
             body = obj['Body'].read()
             self.cache[cache_key] = body
             return body
